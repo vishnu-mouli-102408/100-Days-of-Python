ec3ff4b3e13d1a8c521451250f67c8d6e354eec1
Ganivada Mouli <vishnumouli0@gmail.com>
2023-08-12 09:39:13 +0530

    Day 13

---
+++ Day13/main.py
@@ -0,0 +1,18 @@
+#  Debugging our code
+
+num = int(input("Enter your number"))
+
+if (num % 2) = 0:  # Here instaed of == we gave single = 
+    print('Even')
+else:
+    print ('Odd')
+
+
+num = int(input("Enter your number"))
+
+if (num % 2) == 0:  
+    print('Even')
+else:
+    print ('Odd')
+
+
