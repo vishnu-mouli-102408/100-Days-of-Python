d062926e219797f0d6401d2502dafa3df2e955c3
Ganivada Mouli <vishnumouli0@gmail.com>
2023-08-18 19:15:39 +0530

    Day 19

---
+++ Day19/main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+def move_backward():
+    tim.backward(10)
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+screen.listen()
+screen.onkey(move_forward,"w")
+screen.onkey(move_backward,"b")
+screen.onkey(turn_left,"l")
+screen.onkey(turn_right,"r")
+screen.onkey(clear,"c")
+screen.exitonclick()
